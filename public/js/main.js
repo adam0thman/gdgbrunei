79a7b2e2f558ec4a919d866837dbe526ad2fbfb0
Nik M Adam Hafiz Nik Othman <adam0thman@gmail.com>
2019-10-03 00:09:32 +0800

    Finalizing code sand inline comments for #1 class.

--- public/js/main.js
+++ public/js/main.js
@@ -12,35 +12,43 @@ document.addEventListener('DOMContentLoaded', function () {
   try {
     let app = firebase.app();
     let features = ['auth', 'database', 'messaging', 'storage', 'firestore'].filter(feature => typeof app[feature] === 'function');
-    //document.getElementById('infoload').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+    console.log(`Firebase SDK loaded with ${features.join(', ')}`);
   } catch (e) {
     console.error("Error: " + e);
-    //document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+    console.log('Error loading the Firebase SDK, check the Firebase console.');
   }
-  console.log("ayam gorneg!");
-  $('#divAlert').hide();
 
-  //1. Creating listener to update current highest bid
+  /*
+   ###########################################################################################
+    1. Creating listener to update current highest bid
+    Reference:
+    - https://firebase.google.com/docs/firestore/query-data/listen#events-local-changes
+   ###########################################################################################
+  */
+  // Initialize our firestore call
   const db = firebase.firestore();
+  // We specify our colection ID and the document ID
   db.collection("currentbid").doc("currentbid")
     .onSnapshot({
       // Listen for document metadata changes
       includeMetadataChanges: true
     }, function (bidDoc) {
-      console.log("bidDoc is: " + JSON.stringify(bidDoc.data()));
+      // Afetr querying the document, the return is a javascript object, under variable bidDoc
+      // We can tetst this witha  simpoel console.log as below:
+      console.log(bidDoc.data());
+      // We populatethe 'ámount'into its HTML placeholder
       $('#txtCurrentBid').text(bidDoc.data().amount);
+      // We populatethe 'current bidder' into its HTML placeholder
       $('#txtCurrentBidder').text(bidDoc.data().bidder);
-      setTimeout(function () {
-        $('#txtBidTime').text(bidDoc.data().timestamp.toDate());
-        $('#txtCurrentBid').attr('title', 'Bid placed on ' + bidDoc.data().timestamp.toDate());
-        $('#txtCurrentBid').attr('data-original-title', 'Bid placed on ' + bidDoc.data().timestamp.toDate());
-        $(function () {
-          $('[data-toggle="tooltip"]').tooltip()
-        })
-      }, 1000);
     });
 
-  //2. Adding currentbid to Collection currentbid, Doc currentbid
+  /*
+   ###########################################################################################
+    2. Updating currentbid to Collection currentbid, Document currentbid
+    Reference:
+    - https://firebase.google.com/docs/firestore/manage-data/add-data#update-datas
+   ###########################################################################################
+  */
   // $('#btnBid').on('click', function () {
   //   $('#btnBid').prop('disabled', true);
   //   var bidderName = $('#inputName').val();
@@ -51,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function () {
   //     bidder: bidderName,
   //     timestamp: firebase.firestore.FieldValue.serverTimestamp()
   //   }).then(() => {
-  //     console.log('Done update!');
+  //     console.log('Sucessfully update document with the new highest bidder.s');
   //     $('#inputName').val(null);
   //     setTimeout(function () {
   //       if ($('#inputName').val()) {
@@ -63,8 +71,14 @@ document.addEventListener('DOMContentLoaded', function () {
   //   });
   // });
 
-  //3. Running a transaction to update currentBid and add to list of previous bids
-  //when using this, comment no 2
+  /*
+   ###########################################################################################
+    3. Running a transaction to update currentBid and add to list of previous bids
+    Reference:
+    - https://firebase.google.com/docs/firestore/manage-data/transactions#transactions
+   ###########################################################################################
+  */
+  //when using this, comment out no 2
   $('#btnBid').on('click', function () {
     $('#btnBid').prop('disabled', true);
     var bidderName = $('#inputName').val();
@@ -97,11 +111,16 @@ document.addEventListener('DOMContentLoaded', function () {
     }).catch((e) => {
       console.log('Error updating data: ' + e);
     })
-
-
   });
 
-  //4. Showing previous ten bids: Collection querying
+  /*
+   ###########################################################################################
+    4. Showing previous ten bids: Collection querying
+    Reference:
+    - https://firebase.google.com/docs/firestore/query-data/listen#listen_to_multiple_documents_in_a_collection
+    - https://firebase.google.com/docs/firestore/query-data/queries#compound_queries
+   ###########################################################################################
+  */
   db.collection("allBids").orderBy("timestamp", "desc").limit(10)
     .onSnapshot(function (querySnapshot) {
       //var bidStrings = [];
@@ -109,14 +128,17 @@ document.addEventListener('DOMContentLoaded', function () {
       console.log(querySnapshot)
       $('#prev_bids').empty()
       querySnapshot.forEach(function (doc) {
-        var text = '<small>$' + doc.data().amount + 
-                   ' <text class="text-muted">bid by</text> <text data-toggle="tooltip" data-placement="right" title="Bid made on ' + 
-                   doc.data().timestamp.toDate() + '">' + doc.data().bidder + '</text></small><br />'
+        var text = '<small>$' + doc.data().amount +
+          ' <text class="text-muted">bid by</text> <text data-toggle="tooltip" data-placement="right" title="Bid made on ' +
+          doc.data().timestamp.toDate() + '">' + doc.data().bidder + '</text></small><br />'
         //console.log(text)
         $('#prev_bids').append(text)
       })
     });
-
+  
+  // Monitor the input field with id=#inputName
+  // and only enable the Bid button once user
+  // input their name.
   $('#inputName').on('input', function () {
     if ($('#inputName').val().length > 0) {
       $('#btnBid').prop('disabled', false);
@@ -125,12 +147,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-
 });
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+/*
+  Future addition to this code, for upcoming classes in this Firebase Series:
 
-  //Future: Firebase Auth with admin roles, admins are allowed to change items
+  - Firebase Auth
+  - Security Rules
+  - Firebase Storage
+  - Admin fucntionaloity to changee bid items
+ */
